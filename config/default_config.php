e3195fa0ec95b098d0ee3b3088e84ffeea5dc899
ashaw596 <ashaw596@gmail.com>
2014-01-18 18:23:06 -0500

    set defaultConfig file.

--- config/default_config.php
+++ config/default_config.php
@@ -2,8 +2,9 @@
     $db_server = "localhost";
     $db_username = "root";
     $db_password = "school";
-    $db_database = "mega";
-    $db_userTable = "users";
-    $db_postTable = "posts";
+	$db_database = "evertext";
+    $db_consTable = "conversations";
+	$db_messTable = "messages";
+	$db_userTable = "users";
     $config_evernote_key = "S=s1:U=8db95:E=14afb48ee04:C=143a397c207:P=1cd:A=en-devtoken:V=2:H=20816c4e9d695c40ad4b2df076c32808";
 ?>
