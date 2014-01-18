a33b17db266089ad54cf206a16a65da159fcbb75
praznav <Pranav@gollum.(none)>
2014-01-18 18:39:25 -0500

    callback

--- callback.php
+++ callback.php
@@ -13,10 +13,11 @@ define('LOGIN',"@login");
 
 //logiicccc
 //if statement for whether it is a central number
-if ($recipient == 999999999 ) { // INSERT CENTRAL HERE
+if ($recipient == 9149404409 ) { // INSERT CENTRAL HERE
 
     //if statement for whether it contains new
-    if (strpos($body, NEW) === false ) { // contains new
+	if (strpos($body, constant("NEW")) === false ) {
+    //if(true){
         $to = array($sender);
         $invalidMssg = "sorry. no command found";
         sendMessage($recipient, $to, $invalidMssg);
@@ -27,16 +28,26 @@ if ($recipient == 999999999 ) { // INSERT CENTRAL HERE
 
 } else {    // is a group number
     if (strpos($body, "@") === false) {     // no commands
-        sendToGroup (getConversationID($recipient), $body);
+        $users = getConversationUsers (getConversationID($recipient));
+        if (($key = array_search($sender, $users)) !== false) {
+            unset($users[$key]);
+        }
+        sendMessage($recipient, $users, $body);
     } else {    // commands exist
         if (strpos($body, LEAVE) !== false) {
-            leaveConversation(getConversationID($recipient), $sender);
+            leaveConversation(getConversationID($recipient), getUserID($sender) );
         } else if (strpos($body, SAVE) !== false) {
             saveToEvernote($msg, $groupName, $timeStamp, $authKey);
         }
         while (strpos($body, INVITE) !== false) {
-            $person = substr ($body, strpos($body, INVITE) + 8, (strpos(substr ($body, strpos($body, SAVE) + 8), " ") - strpos($body, INVITE) ) );
-            $body = substr ($body, 0, strpos($body, INVITE) ) . substr($body, strpos($body, INVITE) + 8 );
+            strstr();
+            $dawords = explode(" ", strstr($body, INVITE));
+            for ($af = 0; $af < sizeOf($dawords); $af ++ ) {
+                if ($dawords[af] == INVITE){
+                    $person = $dawords[af+1];
+                }
+            }
+            $body = strstr($body, INVITE) . strstr($person);
             invite($person);
         }
 
