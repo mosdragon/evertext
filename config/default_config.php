82c5893b56e2764b355ff53f9524c8930c28af45
Muhammad Osama Sakhi <msakhi21@gmail.com>
2014-01-18 00:44:25 -0500

    Updated default config php file

--- config/default_config.php
+++ config/default_config.php
@@ -1,8 +1,9 @@
 <?php
-	$db_server = "localhost";
-	$db_username = "root";
-	$db_password = "school";
-	$db_database = "mega";
-	$db_userTable = "users";
-	$db_postTable = "posts";
+    $db_server = "localhost";
+    $db_username = "root";
+    $db_password = "school";
+    $db_database = "mega";
+    $db_userTable = "users";
+    $db_postTable = "posts";
+    $config_evernote_key = "S=s1:U=8db95:E=14afb48ee04:C=143a397c207:P=1cd:A=en-devtoken:V=2:H=20816c4e9d695c40ad4b2df076c32808";
 ?>
