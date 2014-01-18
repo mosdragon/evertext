49522602b40c5343c1e21f73975a27cee8f02a73
Muhammad Osama Sakhi <msakhi21@gmail.com>
2014-01-18 14:08:06 -0500

    Callback.php being worked on

--- callback.php
+++ callback.php
@@ -1,14 +1,21 @@
 <?php
-	$tempString="";
-	 foreach ($_POST as $columnName => $columnData) {
-		$tempString = $tempString.$columnName.": ".$columnData."\n";
-	 }
-	 
-	 file_put_contents("test.txt", $tempString);
-	 
-	 //header("content-type: text/xml");
-    //echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	$body = $_POST['Body'];
+	$recipient = $_POST['To'];
+	$sender = $_POST['From'];
+
+	$msgArray = explode(" ", $body);
+
+for($x = 0, $x<sizeof($msgArray);x++) {
+    if($msgArray[x] == INVITE) {
+        invite($msgArray[x+1]);
+    }
+    elseif($msgArray[x] == LEAVE) {
+        leaveConversation($groupName);
+        break;
+    }
+    elseif($msgArray[x] == SAVE) {
+        saveToEvernote($msg, $groupName, $timeStamp, $authKey);
+    } 
+}
+	sentToAll($body, $sender);
 ?>
-<Response>
-    <Message><?php echo $tempString; ?></Message>
-</Response>
