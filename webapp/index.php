a61cdcc99594330071b05eb9351032ca0eb2222a
Muhammad Osama Sakhi <msakhi21@gmail.com>
2014-01-19 01:59:42 -0500

    formatting

--- webapp/index.php
+++ webapp/index.php
@@ -14,30 +14,10 @@
         texting apps, thumb-cramping copy-paste frenzies, or buried life-changing messages.
         Initiate group chats. Export to EverNote &copy;. Take the world by storm. It's that easy!</p>
 
+        <!-- Modal Toggle -->
         <p><a class="btn btn-lg btn-success" data-toggle="modal" href="#myModal" role="button">
           DO IT
         </a></p>
-        <!-- Modal -->
-     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Enter EverTexts</h4>
-              </div>
-              <div class="modal-body">
-                <form id="phone" action="enter.php" method="post">
-                Phone Number: <br/>
-                <input type="text" name="number"><br />
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" onclick ="$('#phone').submit()">Enter</button>
-              </div>
-            </form>   
-            </div><!-- /.modal-content -->
-          </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
       </div>
       
 
