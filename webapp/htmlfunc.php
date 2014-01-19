5aa7ed091106e1ea224f93393981a1b6ac193a3a
Muhammad Osama Sakhi <msakhi21@gmail.com>
2014-01-19 01:08:27 -0500

    editting header functions

--- webapp/htmlfunc.php
+++ webapp/htmlfunc.php
@@ -36,15 +36,15 @@ function insertHeader($pageTitle) {
 
 }
 function addModal() { ?>
-      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <form id="phone" action="enter.php" method="post">
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title" id="myModalLabel">Enter EverTexts</h4>
               </div>
-              <div class="modal-body">
-                <form id="phone" action="enter.php" method="post">
+              <div class="modal-body"> 
                 Phone Number: <br/>
                 <input type="text" name="number"><br />
               </div>
