268f070a2e3898ec9cc8bc6487969fd80314f11c
praznav <Pranav@gollum.(none)>
2014-01-18 19:26:46 -0500

    defualt config update

--- config/default_config.php
+++ config/default_config.php
@@ -7,4 +7,7 @@
 	$db_messTable = "messages";
 	$db_userTable = "users";
     $config_evernote_key = "S=s1:U=8db95:E=14afb48ee04:C=143a397c207:P=1cd:A=en-devtoken:V=2:H=20816c4e9d695c40ad4b2df076c32808";
-?>
+	$config_twilio_sid = "ACb6da7e66ee47b598e53735fdc209bcd1";
+	$config_twilio_token = "4020b1c25d6a320ad78560ae857be064" ;
+	$config_central_twilio_number = 9149404409;
+	?>
