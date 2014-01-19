4f29b9bd9e172090260ac4d6e24ecc77314beee4
Muhammad Osama Sakhi <msakhi21@gmail.com>
2014-01-19 09:07:35 -0500

    Formatted Jumbotron

--- webapp/index.php
+++ webapp/index.php
@@ -10,7 +10,7 @@
           Hey You
         <img src="images/point_finger.png" height="63px" width="75px" />
         </h1>
-        <p class="lead"> This is it. This is the start of a beautiful journey. No more need for group
+        <p id="mixed"> This is it. This is the start of a beautiful journey. No more need for group
         texting apps, thumb-cramping copy-paste frenzies, or buried life-changing messages.
         Initiate group chats. Export to EverNote &copy;. Take the world by storm. It's that easy!</p>
 		<h3><b>Send a text to 1-313-486-5908 to join the public demo conversation.</b></h3>
